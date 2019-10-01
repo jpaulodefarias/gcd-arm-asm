3475329a56e3052caa8f5796a2dd74f73a850cef
João Paulo de Farias <jpaulodefarias@gmail.com>
2019-10-01 14:36:37 -0300

    Added GCD source file.

---
+++ gcd.s
@@ -0,0 +1,29 @@
+			; Calculates the Greatest Commin Divisor (GCD)
+			
+			bl		init
+			
+init
+			mov		r1, #81 ; Register A.
+			mov		r2, #45 ; Register B.
+			bl		compare
+			end
+			
+compare
+			cmp		r1, r2
+			blgt	subtract_a ; If A is greater than B...
+			bllt	subtract_b ; If B is is lesser than A...
+			end
+			
+subtract_a
+			sub		r1, r1, r2 ; ... A = A - B
+			cmp		r1, r2
+			bne		compare
+			end
+			
+subtract_b
+			sub		r2, r2, r1 ; ... B = B - A
+			cmp		r1, r2
+			bne		compare
+			end
+			
+			; When A equals B, the result was found.
